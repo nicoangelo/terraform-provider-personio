ccd5020f9bf4774c256b397cd7e321ff84c55d25
Nicolas Angelo <nicolas.angelo@gmail.com>
2023-03-18 17:13:53 +0100

    initially add locally mocked api for tests

--- internal/provider/employees_data_source_test.go
+++ internal/provider/employees_data_source_test.go
@@ -1,15 +1,36 @@
 package provider
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/jesse0michael/go-rest-assured/assured"
 )
 
 func TestAccEmployeesDataSource(t *testing.T) {
+	emps, _ := os.ReadFile("../../testdata/all_employees.json")
+	c := assured.NewDefaultClient()
+	c.Given(assured.Call{
+		Path:       "/company/employees",
+		Method:     "GET",
+		StatusCode: 200,
+		Response:   emps,
+	})
+	c.Given(assured.Call{
+		Path:       "/auth",
+		Method:     "POST",
+		StatusCode: 200,
+		Response:   []byte(`{"success": true, "data": { "token": "ghi" } }`),
+	})
+	fmt.Println("Rest assured running on", c.URL())
+	os.Setenv("PERSONIO_API_URL", c.URL())
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+
 		Steps: []resource.TestStep{
 			// Read testing
 			{
